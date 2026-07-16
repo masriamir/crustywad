be6bb2a3280618f0b2dbd3e7b93d95a50bace369
masriamir <masriamir@users.noreply.github.com>
2026-07-16 03:45:21 +0000

    Deploying to gh-pages from @ masriamir/crustywad@22b7f29e93ceb6847fc8ba5b457b932787fa19ab 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ec6f03b6.js',
+                path_to_root + 'searchindex-6de0052b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
